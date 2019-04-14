2d67c984ccd6da39f4634da1d6ed83e61eec8fb0
Isidro Gomes <isidro_gomes@hotmail.co.uk>
2019-04-14 13:18:55 +0100

    Add project files.

---
+++ Wonders/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wonders.Startup))]
+namespace Wonders
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
